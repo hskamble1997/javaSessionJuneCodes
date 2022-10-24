3417b047c1332c2fac15458ce40a936e2faadb7c
hskamble1997 <hskamble1997@gmail.com>
2022-10-24 22:58:46 +0530

    Added a code for checking strings are anagram

---
+++ src/javaPrograms/AnagramTest.java
@@ -0,0 +1,25 @@
+package javaPrograms;
+
+import java.util.Arrays;
+
+public class AnagramTest {
+
+	public static void main(String[] args) {
+		String s1 = "mary";
+		String s2 = "army";
+		
+		char[] arr1 = s1.toLowerCase().toCharArray();
+		char[] arr2 = s2.toLowerCase().toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		if(Arrays.equals(arr1, arr2)) {
+			System.out.println("Given strings are anagram");
+		}else {
+			System.out.println("Given strings are not anagram");
+		}
+
+	}
+
+}
